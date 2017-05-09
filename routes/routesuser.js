205f231255df570fbaeba39cde4b8d93abc5a17a
Yuliia Stelmakhovska <ystelmahovskaya@gmail.com>
2017-05-09 12:42:44 +0200

    routes for statistics response  fixed

--- routes/routesuser.js
+++ routes/routesuser.js
@@ -16,7 +16,7 @@ module.exports = function(app, db) {
  });
     app.post('/Users', function(req, res) {
         console.log("req"+req);
-        const usr = { user_name: req.body.user_name, user_password: req.body.user_password, user_password: req.body.user_score };
+        const usr = { user_name: req.body.user_name, user_password: req.body.user_password, user_score: req.body.user_score };
     db.collection('Users').insert(usr, function(err, result)  {
         if (err) {
             res.send({ 'error': 'An error has occurred' });
@@ -28,8 +28,8 @@ module.exports = function(app, db) {
     app.get('/Statistics/:id', function(req, res) {
 
         const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
-        db.collection('Statistics').findOne(details, function(err, item){
+        const details = { 'user_id': new ObjectID(id) };//todo edit
+        db.collection('Statistics').find(details, function(err, item){
             if (err) {
                 res.send({'error':'An error has occurred'});
             } else {
@@ -38,7 +38,7 @@ module.exports = function(app, db) {
         })
     });
     app.post('/Statistics', function(req, res) {
-        const usr = { _id: req.body._id, date: req.body.date, score: req.body.score };
+        const usr = { user_id: req.body.user_id, date: req.body.date, score: req.body.score };
         db.collection('Statistics').insert(usr, function(err, result)  {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });
